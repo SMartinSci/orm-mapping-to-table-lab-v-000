5536fd2c6908f25b55084b216baf5244e0b9eb13
Sherry Martin <sm674@cornell.edu>
2018-10-20 15:57:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,10 +26,10 @@ class Student
 
   def save
     sql = <<- SQL
-    INSERT INTO songs (name, grade) 
+    INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-    DB[:conn].execute(sql, self.name, self.album)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
 
 
